6f8976f8acb4c97cb3ad331aeb5a2a198561ba58
Anzo52 <andy@zollnersolutions.com>
2022-12-12 23:55:33 -0700

    add termen() and start_menu()

--- cli.py
+++ cli.py
@@ -12,6 +12,58 @@ import simple_term_menu
 # set auth
 
 def cli_auth_api(consumer_key, consumer_secret, access_token, access_token_secret): 
+    """
+    > The function `cli_auth_api` takes four arguments (consumer_key, consumer_secret, access_token,
+    access_token_secret) and returns an object that can be used to make requests to the Twitter API
+    
+    :param consumer_key: The API key generated when you registered your app
+    :param consumer_secret: The consumer secret key for your app
+    :param access_token: The access token you get from the API
+    :param access_token_secret: The access token secret provided by Twitter when you registered your
+    application
+    :return: An object that represents the Twitter API.
+    """
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
+
+
+def termen(opts):
+    """
+    It takes a list of strings and returns the string that the user selects from a menu
+    
+    :param opts: a list of strings, each string is a menu entry
+    :return: The index of the selected option.
+    """
+    term_menu = simple_term_menu.Menu(opts)
+    menu_entry_index = term_menu.show()
+    return opts[menu_entry_index]
+
+def start_menu():
+    """
+    It displays a menu with three options, and then calls the appropriate function based on the user's
+    choice
+    """
+    menu_items = [
+        "Config/Keys", 
+        "Main", 
+        "Exit"
+        ]
+    choice = termen(menu_items)
+    if choice == menu_items[0]:
+        config_menu()
+    elif choice == menu_items[1]:
+        main_menu()
+    else:
+        exit()
+
+# TODO: config_menu()
+def config_menu():
+    pass
+
+# TODO: main_menu()
+def main_menu():
+    pass
+
+if __name__ == "__main__":
+    start_menu()
